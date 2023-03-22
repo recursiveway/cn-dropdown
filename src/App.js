4b05e6d1d45824e9beee78f15668d24f9e1f2adb
Ashutosh Choudhary <ashutoshchoudhary998@gmail.com>
2023-03-22 12:29:38 +0530

    skilltest

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Dropdown from "./components/Dropdown";
+
+function App() {
+
+	// These items would be displayed in the dropdown menu
+	const options = ["Yes", "No", "Probably Not"];
+
+	return (
+		<div className="App">
+			{/* Question */}
+			<h1>Please select one of the option from dropdown</h1>
+			{/* Dropdown Component */}
+			{/* options array is send as props to Dropdown component */}
+			<Dropdown options={options} />
+		</div>
+	);
+}
+
+export default App;
